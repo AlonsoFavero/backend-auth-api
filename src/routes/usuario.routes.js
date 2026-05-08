552ae4ae507953ce17d0b50821af0998226f34f6
AlonsoFavero <alonso.favero@gmail.com>
2026-05-08 16:53:36 -0300

    feat: adicionar rota de perfil e remoção de usuários com controle de acesso por admin

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -4,7 +4,6 @@ const {login} = require("../controllers/auth.controller")
 const {authMiddleware, adminMiddleware} = require("../middlewares/middleware")
 const Usuario = require("../model/usuario.model")
 
-
 const router = Router()
 
 router.get("/perfil", authMiddleware, async (req, res) => {
@@ -19,6 +18,23 @@ router.get("/perfil", authMiddleware, async (req, res) => {
     return res.json({ usuario})
     } catch(error) {
         return res.status(500).json({ error: "erro interno do servidor"})
+    }
+})
+
+router.delete("/usuarios/:id", authMiddleware, adminMiddleware, async(req, res) => {
+     try {
+        const { id } = req.params
+
+        const usuario = await Usuario.findByIdAndDelete(id)
+
+        if (!usuario) {
+            return res.status(404).json({ error: "usuário não encontrado" })
+        }
+
+        return res.json({ message: "usuário deletado com sucesso" })
+
+    } catch (error) {
+        return res.status(500).json({ error: "erro interno do servidor" })
     }
 })
 
