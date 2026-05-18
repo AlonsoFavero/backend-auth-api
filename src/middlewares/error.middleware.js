6b82267cb598db19f9cc2734954c0a77507aab8b
AlonsoFavero <alonso.favero@gmail.com>
2026-05-18 11:28:07 -0300

    feat: melhora tratamento de erros de validação com mensagens da Zod

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -3,7 +3,7 @@ const {ZodError} = require("zod")
 function errorHandler(error, req, res, next){
 
     if(error instanceof ZodError){
-        return res.status(400).json({error:"error de validação "})
+     return res.status(400).json ({error: error.errors[0].message}) 
     }
 
     return res.status(500).json({error: "erro interno no servidor"})
